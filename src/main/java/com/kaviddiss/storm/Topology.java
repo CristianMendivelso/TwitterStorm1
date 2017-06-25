a810e1d2d7064999eb12a682170e8a9981df0540
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-06-25 10:58:50 -0500

    cluster config

--- src/main/java/com/kaviddiss/storm/Topology.java
+++ src/main/java/com/kaviddiss/storm/Topology.java
@@ -2,6 +2,7 @@ package com.kaviddiss.storm;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
 
 /**
@@ -13,7 +14,7 @@ public class Topology {
 
 	static final String TOPOLOGY_NAME = "storm-twitter-word-count";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Config config = new Config();
 		config.setMessageTimeoutSecs(120);
 
@@ -23,16 +24,11 @@ public class Topology {
         b.setBolt("IgnoreWordsBolt", new IgnoreWordsBolt()).shuffleGrouping("WordSplitterBolt");
         b.setBolt("WordCounterBolt", new WordCounterBolt(10, 5 * 60, 50)).shuffleGrouping("IgnoreWordsBolt");
 
-		final LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology(TOPOLOGY_NAME, config, b.createTopology());
-
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				cluster.killTopology(TOPOLOGY_NAME);
-				cluster.shutdown();
-			}
-		});
+		//final LocalCluster cluster = new LocalCluster();
+		//cluster.submitTopology(TOPOLOGY_NAME, config, b.createTopology());
+                config.setNumWorkers(3);
+                StormSubmitter.submitTopology("words", config, b.createTopology());
+		
 
 	}
 
